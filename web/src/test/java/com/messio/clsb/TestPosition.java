e4c152037362a7e655a0baa8ed2a485d15aa5fd0
jpc <jpcuvelliez@gmail.com>
2016-10-20 07:48:41 +0200

    .

--- web/src/test/java/com/messio/clsb/TestPosition.java
+++ web/src/test/java/com/messio/clsb/TestPosition.java
@@ -1,31 +1,26 @@
 package com.messio.clsb;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by jpc on 29-09-16.
  */
 public class TestPosition {
     @Test
-    public void testAddition(){
+    public void testAdditionAndNegate(){
         final Position p1 = new Position("EUR", 10, "USD", 5);
         final Position p2 = new Position("USD", 15, "JPY", 100);
-        System.out.println(p1.add(p2));
-        System.out.println(p2.add(p1));
-        System.out.println(p2.negate());
+        assertEquals(p1.add(p2), new Position("EUR", 10, "USD", 20, "JPY", 100));
+        assertEquals(p2.add(p1), new Position("EUR", 10, "USD", 20, "JPY", 100));
+        assertEquals(p2.negate(), new Position("USD", -15, "JPY", -100));
     }
 
     @Test
     public void testShortAndLong(){
         final Position p1 = new Position("EUR", 10, "USD", -5, "JPY", 100);
-        System.out.println(p1.xshort());
-        System.out.println(p1.xlong());
+        assertEquals(p1.xshort(), new Position("USD", -5));
+        assertEquals(p1.xlong(),  new Position("EUR", 10, "JPY", 100));
     }
 
-    @Test
-    public void testNegate(){
-        final Position p1 = new Position("EUR", 10, "USD", -5, "JPY", 100);
-        System.out.println(p1.negate());
-
-    }
 }
