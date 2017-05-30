abdd5337ed7c30b0835fced5780fd26004791552
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2017-05-30 15:49:40 +0200

    .

--- clits/src/main/resources/META-INF/resources/app/overview.component.ts
+++ clits/src/main/resources/META-INF/resources/app/overview.component.ts
@@ -1,16 +1,17 @@
 import {Component, OnInit} from "@angular/core";
 import {ClsbService} from "./clsb.service";
+import {Observable} from "rxjs/Rx";
 
 @Component({
     selector: 'overview',
     template: `
-<svg [attr.viewBox]="['0 0 1440 ', (currencies.length + 2) * hu].join('')" version = "1.1" xmlns="http://www.w3.org/2000/svg">
+<svg [attr.viewBox]="['0 0 ', 24 * wu ,' ', (currencies.length + 2) * hu].join('')" version = "1.1" xmlns="http://www.w3.org/2000/svg">
     <line x1="0" y1="0" [attr.x2]="24 * wu" y2="0"/>
     <g *ngFor="let h of hours; let i = index">
         <line [attr.x1]="i * wu" y1="0" [attr.x2]="i * wu" [attr.y2]="hu"/>
         <text [attr.x]="i * wu + 2" y="10" class="text-small">{{i}}</text>
     </g>
-    <g *ngIf="bank" transform="translate(0,20)">
+    <g *ngIf="bank" [attr.transform]="['translate(0,', hu,')'].join('')">
         <rect [attr.x]="time(bank.opening)" y="0" [attr.width]="time(bank.closing) - time(bank.opening)" [attr.height]="hu" fill="aquamarine"/>
         <line [attr.x1]="time(bank.settlementCompletionTarget)" y1="0" [attr.x2]="time(bank.settlementCompletionTarget)" [attr.y2]="hu" class="sct"/>
     </g>
@@ -59,15 +60,11 @@ export class OverviewComponent implements OnInit {
     };
 
     ngOnInit(): void {
-        this.service.getCurrencies().subscribe(
-            o => {
-                this.currencies = o;
-            }
-        );
-        this.service.getBank().subscribe(
-            o => {
-                console.info('bank', JSON.stringify(o));
-                this.bank = o;
+        Observable.forkJoin(this.service.getCurrencies(), this.service.getBank()).subscribe(
+            (ar: any[]) => {
+                console.info('ar', JSON.stringify(ar));
+                this.currencies = ar[0];
+                this.bank = ar[1];
             }
         );
     }
