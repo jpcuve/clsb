cabb28d2d256dfac55a9dc8977cacf8de85c9164
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2024-09-12 11:05:52 +0200

    .

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {Box, Button } from '@mantine/core'
+
+function App() {
+  return (
+    <Box p="sm">
+      <Button>Test</Button>
+    </Box>
+  )
+}
+
+export default App
