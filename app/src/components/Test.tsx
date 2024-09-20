8a1a022bfbdb0acf17a8f5b697c8cf7cee14c578
jpcuve <jpcuvelliez@gmail.com>
2024-09-20 21:46:57 +0200

    .

---
+++ app/src/components/Test.tsx
@@ -0,0 +1,10 @@
+import {Box} from '@mantine/core'
+import {FC} from 'react'
+
+const Test: FC = () => {
+  return (
+    <Box p="sm">Test</Box>
+  )
+}
+
+export default Test
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,17 +5,21 @@ import App from './App.tsx'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import {StompSessionProvider} from 'react-stomp-hooks'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider} from 'react-router-dom'
 import Admin from './components/Admin.tsx'
 import Dashboard from './components/Dashboard.tsx'
 import {Provider} from 'react-redux'
 import {store} from './store.ts'
+import Test from './components/Test.tsx'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<App/>}>
+    <Route index element={<Navigate to="/" replace/>}/>
+    <Route path="test" element={<Test/>}/>
     <Route path="admin" element={<Admin/>}>
       <Route path="dashboard" element={<Dashboard/>}/>
     </Route>
+    <Route path="*" element={<Navigate to="/" replace/>}/>
   </Route>
 ), {
   basename: import.meta.env.VITE_APP_WEB_CONTEXT,
